01d55b7e57d758945d3e05fcd5735b8bbbadcc29
beillrebecca <noanh0810@gmail.com>
2026-03-03 07:29:11 +0900

    script.js

--- script.js
+++ script.js
@@ -5,4 +5,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const showcase = document.getElementById("showcase");
   console.log("showcase:", showcase);
+  let items = [];
+
+for (let i = 1; i <= 12; i++) {
+  items.push({
+    name: "アイテム" + i,
+    img: "https://dummyimage.com/300x300/eeeeee/999999&text=%F0%9F%93%B7",
+    link: "商品リンク",
+    clicks: 0
+  });
+}
+
+console.log("items準備完了", items);
 });

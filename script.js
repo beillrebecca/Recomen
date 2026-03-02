db162747d2c110da026e80aa1fcf2aad54e36487
beillrebecca <noanh0810@gmail.com>
2026-03-03 07:25:26 +0900

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-console.log("STEP1");
+console.log("Recomen JS 起動");
 
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("DOM OK");
+  console.log("DOM読み込みOK");
 
   const showcase = document.getElementById("showcase");
   console.log("showcase:", showcase);

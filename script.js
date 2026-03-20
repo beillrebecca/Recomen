95c5e39b38a759e6031769d3b29dad908e8e9b46
beillrebecca <noanh0810@gmail.com>
2026-03-20 14:54:47 +0900

    script.js

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 alert("JSは読み込まれた！");
 console.log("JSは読み込まれた！");
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM読み込みOK");
+
+  const showcase = document.getElementById("showcase");
+  const itemImgInput = document.getElementById("itemImgInput");
+
+  console.log('showcase:', showcase);
+  console.log('itemImgInput:', itemImgInput);
+
+  if (!showcase) {
+    console.error("showcase が見つからない");
+    return;
+  }
+
+  if (!itemImgInput) {
+    console.warn("itemImgInput が見つからない");
+  }
+});

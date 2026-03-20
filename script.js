4658627e2e3fd7e5069af4bd2ca728c564cd0c03
beillrebecca <noanh0810@gmail.com>
2026-03-20 17:34:00 +0900

    script.js

--- script.js
+++ script.js
@@ -2,13 +2,30 @@ alert("JS読み込まれてる！");
 
 const showcase = document.getElementById("showcase");
 
-console.log("showcase:", showcase);
-
 if (showcase) {
   showcase.addEventListener("click", (e) => {
-    console.log("クリックされた！", e.target);
-    alert("カードがクリックされた！");
+
+    // ❤️ ハートだけ反応させる
+    const heart = e.target.closest(".icon-heart");
+
+    if (heart) {
+      heart.classList.toggle("liked");
+
+      const path = heart.querySelector("path");
+
+      if (path) {
+        if (heart.classList.contains("liked")) {
+          path.setAttribute("fill", "red");
+          path.setAttribute("stroke", "red");
+        } else {
+          path.setAttribute("fill", "none");
+          path.setAttribute("stroke", "#000");
+        }
+      }
+
+      console.log("ハート押された");
+      return;
+    }
+
   });
-} else {
-  console.error("showcase取れてない");
 }

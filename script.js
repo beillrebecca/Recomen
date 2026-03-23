f3067bbe6c81c3d3bfa3a780dbf4c3701c242318
beillrebecca <noanh0810@gmail.com>
2026-03-23 23:52:24 +0900

    script.js

--- script.js
+++ script.js
@@ -285,7 +285,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const rect = btn.getBoundingClientRect();
 
-  const top = rect.top + rect.height - popup.offsetHeight + 4;
+  const top = rect.top + rect.height - popup.offsetHeight + 10;
   const left = rect.left + rect.width / 2 - popup.offsetWidth / 2;
 
   popup.style.position = "fixed";

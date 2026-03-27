c79e88027c37bd278e25b9ac28495186d07732e3
beillrebecca <noanh0810@gmail.com>
2026-03-27 09:50:53 +0900

    script.js

--- script.js
+++ script.js
@@ -502,7 +502,7 @@ function positionPopup(btn, popup) {
   let left = rect.left;
 
   // 👇ここがポイント
-  let top = rect.bottom + 4;
+  let top = rect.bottom + 20;
 
   left = Math.max(8, Math.min(left, window.innerWidth - popupWidth - 8));
 

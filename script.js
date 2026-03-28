2fceb3bf207331cef8b99d4f203b9bb3faf2696d
beillrebecca <noanh0810@gmail.com>
2026-03-29 00:17:33 +0900

    script.js

--- script.js
+++ script.js
@@ -635,10 +635,4 @@ document.addEventListener("click", () => {
     followersBtn?.addEventListener('click',()=>showModal('followers'));
     closeBtn?.addEventListener('click',()=>modal.style.display='none');
   }
-
-  // =========================
-  // 共通画像アップロード
-  // =========================
-  setupImageUpload(document.getElementById('headerImg'), document.getElementById('headerImgInput'));
-  setupImageUpload(document.getElementById('avatarImg'), document.getElementById('avatarImgInput'));
 });

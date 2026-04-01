0c4ea2c957f08d9eae826ea0a3479bfc90e88018
beillrebecca <noanh0810@gmail.com>
2026-04-01 22:13:39 +0900

    script.js

--- script.js
+++ script.js
@@ -68,13 +68,10 @@ function saveIcon(item) {
 
 function deleteIcon() {
   return `
-    <svg class="icon-delete" viewBox="0 0 24 24" stroke-width="1.5"
+    <svg class="icon-delete" viewBox="0 0 24 24" stroke-width="2"
       stroke-linecap="round" stroke-linejoin="round" fill="none">
-      <path d="M3 6h18"/>
-      <path d="M8 6V4h8v2"/>
-      <path d="M19 6l-1 14H6L5 6"/>
-      <path d="M10 11v6"/>
-      <path d="M14 11v6"/>
+      <line x1="18" y1="6" x2="6" y2="18"/>
+      <line x1="6" y1="6" x2="18" y2="18"/>
     </svg>
   `;
 }
